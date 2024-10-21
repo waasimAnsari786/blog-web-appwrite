3be5aa7874daa0b74a8637df77ad8c5d170ba0b2
waasimAnsari786 <waasimansari92@gmail.com>
2024-10-20 22:26:28 -0700

    i configured redux toolkit

---
+++ src/components/MyWeb.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import authService from "../appwrite/auth";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/authSlice";
+
+export default function MyWeb() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    <div>loading...</div>;
+  }
+
+  return (
+    <>
+      <Header />
+      <main>
+        <p>outlet</p>
+      </main>
+      <Footer />
+    </>
+  );
+}
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducers from "../features/authSlice";
+
+const store = configureStore({
+  reducer: authSliceReducers,
+});
+
+export default store;
